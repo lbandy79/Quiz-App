f7d57d3255f0c2deebae9c1c12cf75b4335d8b5b
Luke Bandy <luke.bandy@gmail.com>
2018-04-09 12:57:37 -0400

    Inital Commit

---
+++ Quiz App/app.js
@@ -0,0 +1,171 @@
+$(document).ready(() => {
+
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+
+    function buildQuiz() {
+        // we'll need a place to store the HTML output
+        const output = [];
+
+        // for each question...
+        myQuestions.forEach(
+            (currentQuestion, questionNumber) => {
+
+                // we'll want to store the list of answer choices
+                const answers = [];
+
+                // and for each available answer...
+                for (letter in currentQuestion.answers) {
+
+                    // ...add an HTML radio button
+                    answers.push(
+                        `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+                    );
+                }
+
+                // add this question and its answers to the output
+                output.push(
+                    `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+                );
+            });
+
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join('');
+    }
+
+    function showResults() {
+
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll('.answers');
+
+        // keep track of user's answers
+        let numCorrect = 0;
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+
+            // find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = 'input[name=question' + questionNumber + ']:checked';
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            // if answer is correct
+            if (userAnswer === currentQuestion.correctAnswer) {
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                answerContainers[questionNumber].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else {
+                // color the answers red
+                answerContainers[questionNumber].style.color = 'red';
+            }
+        });
+    }    
+
+    let myQuestions = [{
+        boxArt: document.getElementById('question1'),
+        question: "Guess the game?",
+        answers: {
+            a: "Casino Royal",
+            b: "What Stays in Vegas",
+            c: "Black Jack"
+        },
+        correctAnswer: "Black Jack",
+    }, {
+        boxArt: document.getElementById('question2'),
+        question: "Guess the game?",
+        answers: {
+            a: "Air Sea Battle",
+            b: "Mommy and Daddy are Fighting Again",
+            c: "World War III"
+        },
+        correctAnswer: "Air Sea Battle",
+    }, {
+        boxArt: document.getElementById('question3'),
+        question: "Guess the game?",
+        answers: {
+            a: "Salvador Dali's Math Brigade",
+            b: "Basic Math",
+            c: "Feak Out the Frog"
+        },
+        correctAnswer: "Basic Math",
+    }, {
+        boxArt: document.getElementById('question4'),
+        question: "Guess the game?",
+        answers: {
+            a: "High School Prom",
+            b: "Take This, Stalin!",
+            c: "Combat"
+        },
+        correctAnswer: "Combat",
+    }, {
+        boxArt: document.getElementById('question5'),
+        question: "Guess the game?",
+        answers: {
+            a: "That Racing Movie With Tom Cruise: The Game",
+            b: "Indy 500",
+            c: "Drive-Thru Heads"
+        },
+        correctAnswer: "Indy 500",
+    }, {
+        boxArt: document.getElementById('question6'),
+        question: "Guess the game?",
+        answers: {
+            a: "Jefferson Airplane",
+            b: "Jefferson Star Ship",
+            c: "Star Ship"
+        },
+        correctAnswer: "Star Ship",
+    }, {
+        boxArt: document.getElementById('question7'),
+        question: "Guess the game?",
+        answers: {
+            a: "Old Timey Car Show",
+            b: "Street Racer",
+            c: "Motorcycle vs Car"
+        },
+        correctAnswer: "Street Racer",
+    }, {
+        boxArt: document.getElementById('question8'),
+        question: "Guess the game?",
+        answers: {
+            a: "Block Pilot",
+            b: "Internet Search History",
+            c: "Surround"
+        },
+        correctAnswer: "Surround",
+    }, {
+        boxArt: document.getElementById('question9'),
+        question: "Guess the game?",
+        answers: {
+            a: "Friday the 13th",
+            b: "SPORTS!",
+            c: "Video Olympics"
+        },
+        correctAnswer: "Video Olympics",
+    }, {
+        boxArt: document.getElementById('question9'),
+        question: "Guess the game?",
+        answers: {
+            a: "3-D Tic-Tac-Toe",
+            b: "Grid Wars",
+            c: "Disco Space"
+        },
+        correctAnswer: "3-D Tic-Tac-Toe",
+    }, ];
+
+     // display quiz right away
+    buildQuiz();
+
+    // on submit, show results
+    submitButton.addEventListener('click', showResults);
+});
